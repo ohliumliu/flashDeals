8baba690f35e400894e733dff77c3c70672fb189
Liu Yu <yul.liuyu@gmail.com>
2015-12-20 23:18:17 -0800

    Set up flashDeals RoR app with product table by scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlashDeals::Application.config.secret_token = 'd442aa152d77557cae76f4aa87d7e769de9c1bb394d505b1247f2912f693bf88932822c58bd2c0015afc7b13bc3870c7016aa940afcf3d485c5d96c5380bc789'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FlashDeals::Application.config.session_store :cookie_store, key: '_flashDeals_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FlashDeals::Application.config.session_store :active_record_store
