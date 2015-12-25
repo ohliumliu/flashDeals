0ef706d2fb355d7067a4f06e937f04d9ab31ad00
Yu Liu <yul.liuyu@gmail.com>
2015-12-24 20:01:27 -0800

    use css from dealsout.net directly

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151223072905) do
+ActiveRecord::Schema.define(:version => 20151224233425) do
 
   create_table "catalogs", :force => true do |t|
     t.string   "name"
     t.string   "url"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "flashdeals_tests", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
