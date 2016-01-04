08457fee05d0697682ae36d5742cb9dcdc17ece6
Yu Liu <yul.liuyu@gmail.com>
2016-01-03 21:38:51 -0800

    add browse_node_id to catalog

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160103090545) do
+ActiveRecord::Schema.define(:version => 20160104053615) do
 
   create_table "catalogs", :force => true do |t|
     t.string   "name"
     t.string   "url"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+    t.string   "browse_node_id"
   end
 
   create_table "flashdeals_tests", :force => true do |t|
