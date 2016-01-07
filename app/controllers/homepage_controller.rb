9f9af45958e7ee3e55fb0f6041bd51b7d3144308
Yu Liu <yul.liuyu@gmail.com>
2016-01-06 19:29:44 -0800

    sort topsellers by percentage_saved

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,6 +3,6 @@ class HomepageController < ApplicationController
     @catalogs = Catalog.all
     @merchants = Merchant.all
     @travel_sites = TravelSite.all
-    @products = Product.all
+    @products = Product.order('percentage_saved DESC')
   end
 end
