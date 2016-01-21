68fe236f1ae4e8a9134eed2a05e480c6a7a99a23
Yu Liu <yul.liuyu@gmail.com>
2016-01-20 23:46:19 -0800

    show user profile after signin

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -17,12 +17,12 @@ class UserController < ApplicationController
   end
 
   def signin
-    user = User.where(:name => params[:username]).first
-    if Digest::SHA1.hexdigest(params[:password]) == user.password
-      flash[:success] = "you are in"
+    @user = User.where(:name => params[:username]).first
+    if (@user = User.where(:name => params[:username]).first) && Digest::SHA1.hexdigest(params[:password]) == @user.password
+      #flash[:success] = "you are in"
       @products = Product.all
     else
-      redirect_to "/user/signup", :flash => {:error => "I don't know you"}
+      redirect_to action: "signup", :flash => {:error => "I don't know you"} 
     end
        
   end
